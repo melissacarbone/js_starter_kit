262bcb1166452dd8a3463a5f5e79702a5c626347
Melissa Carbone <mcarbone1@gmail.com>
2014-01-19 21:58:14 -0500

    User makes a guess, gets feedback

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
-alert('Hey find me to know where to put the javascript');
+alert('Guess a number between 0 and 100');
+
+$(document).ready(function(){
+
+  $('#guess').keydown(function(e){
+    var number = 30
+    if (e.keyCode == 13) {
+        var guess = $('#guess').val();
+
+      if(number < guess)
+      {
+        $('.alert-box').html(guess + ' ' + 'is too high')
+      }
+      else if (number > guess)
+      {
+        $('.alert-box').html(guess + ' ' + 'is too low');
+      }
+      else
+      {
+        alert('Yes it was' + ' ' + guess + '!' + ' ' + 'You Win!')
+      }
+    }
+});
+
+});
+
